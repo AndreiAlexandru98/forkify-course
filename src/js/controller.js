847d062667cbb80da113847db4044b2b06416ca3
AndreiAlexandru98 <66018335+AndreiAlexandru98@users.noreply.github.com>
2022-04-05 19:39:01 +0300

    Added log

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,6 +130,7 @@ const init = function() {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
+  console.log('Application started');
 }
 
 
